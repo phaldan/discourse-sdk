4592587258c81240f5a6b625c7b5c0800407f164
Philipp Daniels <philipp.daniels@gmail.com>
2017-05-25 15:14:02 +0200

    Fix coverage of Topics

--- tests/Client/Rest/TopicsTest.php
+++ tests/Client/Rest/TopicsTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * @author Philipp Daniels <philipp.daniels@gmail.com>
- * @coversNothing
+ * @covers \PhALDan\Discourse\Client\Rest\Topics
  */
 class TopicsTest extends TestCase
 {
